3ecca01d8b5b528bb813c8a922aff4036eec268b
Divy-gith <divyar2686@gmail.com>
2025-10-08 15:07:45 +0530

    Add files via upload

---
+++ 25bch068B.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  char str[50];
+  printf("enter a string\n");
+  gets(str);
+  void prntstr(char []);
+  prntstr(str);
+  getch();
+}
+  void prntstr(char s[])
+    {
+        char *ptr=s;
+        while (*ptr!='\0')
+    {
+        printf("%c\n ",*ptr++);
+
+    }
+    printf("\nLength of the string is %d.\n",ptr-s);
+    }
